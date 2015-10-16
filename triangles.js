1b00897d808ac9aa19a69e1408be5c2a90644d08
Margo <margo.smith@venmo.com>
2015-10-16 18:37:00 -0400

    change grid, change entire program to use new grid

--- triangles.js
+++ triangles.js
@@ -1,11 +1,9 @@
 var height = window.innerHeight;
 var width = window.innerWidth;
-var triangleDim = 50;
+var triangleDim = 150;
 var triangleHeight = Math.sqrt(3/4) * triangleDim;
-var gridWidth = Math.ceil(width / triangleDim * 3 / 2);
+var gridWidth = Math.ceil(width / triangleDim);
 var gridHeight = Math.ceil(height / triangleHeight);
-var centerX = Math.floor(gridWidth / 2);
-var centerY = Math.floor(gridHeight / 2);
 
 var svg = document.getElementsByTagName('svg')[0];
 svg.setAttribute("width", width);
@@ -13,179 +11,129 @@ svg.setAttribute("height", height);
 svg.setAttribute("onclick", "clearInterval(triangleMaker)");
 
 var triangleMaker;
-var triangles = [];
 var grid = createGrid();
-var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
-var currentTriangle = {
-  gridPoint: grid[centerY][centerX],
-  orientation: "left",
-  fill: "red"
-};
-
-drawTriangle(currentTriangle);
+var currentTriangle = randomTriangle();
+drawTriangle(currentTriangle, "red");
 
 triangleMaker = setInterval(addTriangle, 300);
 
-// showGrid();
+// showGrid(grid);
+
+function getSVGPathString(verts) {
+  var SVGPathString = verts.reduce(function(previous, current) {
+    return previous + " L " + current;
+  });
+  return "M " + SVGPathString + " z";
+}
 
 function createGrid() {
-  var grid = [];
-  for (var i = 0; i <= gridHeight; i++) {
-    var row = [];
-    for (var j = 0; j <= gridWidth; j++) {
-      var offset = 0
-      if (i > 0) {
-        offset = grid[i - 1][0].x - triangleDim / 2;
-      }
-      var point = {
-        left: false,
-        right: false,
-        id: [i, j],
-        x: offset + j * triangleDim,
-        y: i * triangleHeight
+  var grid = {}
+  var y = 0;
+  var j = 0;
+  while ( y < height ) {
+    var x = j % 2 === 0 ? 0 : -triangleDim / 2;
+    var i = 0;
+    while ( x < width ) {
+      var upVerts = [ x + " " + y,
+        (x - triangleDim / 2) + " " + (y + triangleHeight),
+        (x + triangleDim / 2) + " " + (y + triangleHeight) ];
+      var downVerts = [ x + " " + y,
+        (x + triangleDim) + " " + y,
+        (x + triangleDim / 2) + " " + (y + triangleHeight) ];
+      var up = i + "-" + j + "-up";
+      var down = i + "-" + j + "-down";
+      grid[up] = {
+        fill: "none",
+        path: getSVGPathString(upVerts)
       };
-      row.push(point);
+      grid[down] = {
+        fill: "none",
+        path: getSVGPathString(downVerts)
+      };
+      x += triangleDim;
+      i += 1;
     }
-    grid.push(row);
+    y += triangleHeight;
+    j += 1;
   }
-
   return grid;
 }
 
-function showGrid() {
-  var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  svg.appendChild(g);
-
-  for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid[0].length; j++) {
-      var point = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-      point.setAttribute("cx", grid[i][j].x);
-      point.setAttribute("cy", grid[i][j].y);
-      point.setAttribute("r", 3);
-      point.setAttribute("id", "grid_" + i);
-      point.style.fill = "red";
-      g.appendChild(point);
-
-      var text = document.createElementNS("http://www.w3.org/2000/svg", "text")
-      text.setAttribute("x", grid[i][j].x);
-      text.setAttribute("y", grid[i][j].y);
-      text.textContent = "(" + i + ", " + j + ")";
-      g.appendChild(text);
+function showGrid(grid) {
+  for ( var triangle in grid ) {
+    if ( grid.hasOwnProperty(triangle) ) {
+      drawTriangle(triangle, "black");
     }
   }
 }
 
+function randomTriangle() {
+  var i = Math.floor(Math.random() * gridWidth);
+  var j = Math.floor(Math.random() * gridHeight);
+  var orientation = Math.random() > 0.5 ? "up" : "down";
+  var triangle = i + "-" + j + "-" + orientation;
+  if ( grid[triangle].fill === "none" ) {
+    return triangle;
+  } else {
+    return randomTriangle();
+  }
+}
 
 function addTriangle() {
-
-  var x = currentTriangle.gridPoint.id[0];
-  var y = currentTriangle.gridPoint.id[1];
+  var location = currentTriangle.split("-");
+  var x = location[0];
+  var y = location[1];
+  var orientation = location[2];
 
   var options = [];
-  if (currentTriangle.orientation === "left") {
-    options = [[x, y, "right"], [x + 1, y, "right"], [x, y - 1, "right"]];
+  if (orientation === "up") {
+    if ( y % 2 === 0 ) {
+      var complicatedOption = Number(x) + "-" + (Number(y) + 1) + "-down";
+    } else {
+      var complicatedOption = (Number(x) - 1) + "-" + (Number(y) + 1) + "-down";
+    }
+    options = [ Number(x) + "-" +  Number(y) + "-down",
+      (Number(x) - 1) + "-" +  Number(y) + "-down",
+      complicatedOption ];
   } else {
-    options = [[x, y, "left"], [x - 1, y, "left"], [x, y + 1, "left"]];
+    if ( y % 2 === 0 ) {
+      var complicatedOption = ((Number(x) + 1) + "-" + (Number(y) - 1)) + "-up";
+    } else {
+      var complicatedOption = Number(x) + "-" + (Number(y) - 1) + "-up";
+    }
+    options = [ Number(x) + "-" +  Number(y) + "-up",
+      (Number(x) + 1) + "-" +  Number(y) + "-up",
+      complicatedOption ];
   }
 
-  var availOptions = options.filter(isInBoundsAndNotPrevious);
-
-  if (availOptions.length === 0) {
-    currentTriangle = triangles[Math.floor(Math.random() * triangles.length)];
-    addTriangle();
-  }
+  var availOptions = options.filter(function(option) {
+    if ( grid[option] ) {
+      if ( grid[option].fill === "none" ) {
+        return true;
+      }
+    }
+    return false;
+  });
 
   var nextTriangle = availOptions[Math.floor(Math.random() * availOptions.length)];
-  var fillIndex = colors.indexOf(currentTriangle.fill);
-  if ( isFilled(nextTriangle) ) {
-    fillIndex += 1;
-    if ( fillIndex >= colors.length ) {
-      fillIndex = 0;
-    }
-  }
-  var x = nextTriangle[0];
-  var y = nextTriangle[1];
 
-  if (currentTriangle.orientation === "left") {
-    currentTriangle = {
-      gridPoint: grid[x][y],
-      orientation: "right",
-      fill: colors[fillIndex]
-    };
-    drawTriangle(currentTriangle);
-  } else {
-    currentTriangle = {
-      gridPoint: grid[x][y],
-      orientation: "left",
-      fill: colors[fillIndex]
-    };
-    drawTriangle(currentTriangle);
+  if ( !nextTriangle ) {
+    nextTriangle = randomTriangle();
   }
+
+  currentTriangle = nextTriangle;
+  drawTriangle(currentTriangle, "red");
 }
 
-function isInBoundsAndNotPrevious(option) {
-  var x = option[0];
-  var y = option[1];
-  var orientation = option[2];
-
-  var inBounds = true;
-  var notPrevious = true;
-
-  if ( triangles.length > 1 ) {
-    var previousTriangle = triangles[triangles.length - 2];
-    var prevX = previousTriangle.gridPoint.id[0];
-    var prevY = previousTriangle.gridPoint.id[1];
-    var prevOrientation = previousTriangle.orientation;
-  }
-
-  if ( grid[x] === undefined ) {
-    inBounds = false;
-  } else if ( grid[x][y] === undefined ) {
-    inBounds = false;
-  } else if ( x === prevX && y === prevY && orientation === prevOrientation ) {
-    notPrevious = false;
-  }
-
-  return inBounds && notPrevious;
-}
-
-function isFilled(option) {
-  var x = option[0];
-  var y = option[1];
-  var orientation = option[2];
-
-  var filled = false;
-
-  if ( grid[x][y][orientation] ) {
-    filled = true;
-  }
-
-  return filled;
-}
-
-function drawTriangle(triangle) {
-  var rotation;
-  if (triangle.orientation === "left") {
-    rotation = 0;
-    grid[triangle.gridPoint.id[0]][triangle.gridPoint.id[1]].left = true;
-  } else {
-    rotation = 60;
-    grid[triangle.gridPoint.id[0]][triangle.gridPoint.id[1]].right = true;
-  }
-
-  var verts = [ {x: triangle.gridPoint.x, y: triangle.gridPoint.y},
-    {x: triangle.gridPoint.x - triangleDim / 2, y: triangle.gridPoint.y + triangleHeight},
-    {x: triangle.gridPoint.x + triangleDim / 2, y: triangle.gridPoint.y + triangleHeight} ];
+function drawTriangle(triangle, fill) {
+  grid[triangle].fill = fill;
 
   var svgTriangle = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  svgTriangle.setAttribute("d", "M " + verts[0].x + " " + verts[0].y + " L " + verts[1].x + " " + verts[1].y + " L " + verts[2].x + " " + verts[2].y + " z");
-  svgTriangle.setAttribute("transform", "rotate(" + rotation + ", " + verts[2].x + ", " + verts[2].y + ")")
+  svgTriangle.setAttribute("d", grid[triangle].path);
+  svgTriangle.setAttribute("class", triangle);
   svgTriangle.style.stroke = "white";
   svgTriangle.style['stroke-width'] = "5px";
-  svgTriangle.style.fill = triangle.fill;
+  svgTriangle.style.fill = fill;
   svg.appendChild(svgTriangle);
-
-  triangles.push(triangle);
 }
-
